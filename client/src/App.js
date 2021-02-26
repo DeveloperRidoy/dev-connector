0cfbb4ef8f61da1c52c378a920037b9283b09aea
developerridoy <freelancerridoy247@gmail.com>
2021-02-26 23:40:08 +0600

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,16 @@ import './App.css';
 import { useEffect } from 'react';
 import Landing from './components/layouts/Landing';
 import Navbar from './components/layouts/Navbar';
-import Dashboard from './components/layouts/dashboard/Dashboard';
 import { BrowserRouter as Router, Route, Switch }  from 'react-router-dom'
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Profile from './components/layouts/Profile';
-import Profiles from './components/layouts/Profiles';
-import { loadUser } from './store/actions/auth';
-import setAuthToken from './utils/setAuthToken';
-import Alert from "./components/layouts/Alert";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import removeAuthToken from "./utils/removeAuthToken";
-// redux
+
+import Routes from './components/routing/Routes';
+
+// redux store
 import { Provider } from 'react-redux';
 import store from './store/store';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import Posts from './components/layouts/posts/Posts';
-import Post from './components/layouts/Post/Post';
-import NofFound from './components/layouts/NofFound';
+import { loadUser } from "./store/actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+import removeAuthToken from "./utils/removeAuthToken";
 
 
 // set x-auth-token in axios from localStorage
@@ -43,36 +32,10 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Navbar />
-        <Route exact path="/" component={Landing} />
-        <div className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/profiles" component={Profiles} />
-            <PrivateRoute exact path="/profile/:userID" component={Profile} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            <PrivateRoute
-              exact
-              path="/create-profile"
-              component={CreateProfile}
-            />
-            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-            <PrivateRoute
-              exact
-              path="/add-experience"
-              component={AddExperience}
-            />
-            <PrivateRoute
-              exact
-              path="/add-education"
-              component={AddEducation}
-            />
-            <PrivateRoute exact path="/post/:postID" component={Post} />
-            <PrivateRoute exact path="/posts" component={Posts} />
-          </Switch>
-        </div>
-        <Route component={NofFound} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route component={Routes}/>
+        </Switch>
       </Router>
     </Provider>
   );
--- client/src/components/layouts/NofFound.js
+++ client/src/components/layouts/NofFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NofFound = () => {
     return (
         <div className=" p-3">
-            <h1 className="x-large text-primary"> <i className="fas fa-exclamation-triangle"></i> Page Not Found</h1>
+            <h4 className="large text-primary"> <i className="fas fa-exclamation-triangle"></i> Page Not Found</h4>
             <Link to="/">Back to home</Link>
         </div>
     )
