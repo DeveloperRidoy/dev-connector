33c35767e02031fc79d44691d653cc276002b722
developerridoy <freelancerridoy247@gmail.com>
2021-02-26 22:28:57 +0600

    changing hardcoded port from 5000 to PORT

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ if (process.env.NODE_ENV = 'production') {
 
 const PORT = process.env.PORT || 5000; 
  
-app.listen(5000, () => console.log(`server running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
                
